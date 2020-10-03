b2714aa6b7360ab5432c9f054c505642a077b3f2
canodelacuadra <canodelacuadra@gmail.com>
2020-10-03 08:58:15 +0200

    el programa php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include 'programa.php' ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frases Celebres</title>
+    <link rel='stylesheet'href='style.css'>
+   
+</head>
+
+<body>
+<aside class='frase'>
+<?php echo frasescelebres()?>
+</aside>    
+</body>
+</html>
